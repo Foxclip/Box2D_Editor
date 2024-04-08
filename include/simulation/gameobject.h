53b4f04c404998baaecbc0a79574aebd71427300
Foxclip <destroyApp2@gmail.com>
2024-04-08 18:29:29 +1000

    Refactor: move LineStripShape and CircleNotchShape to shapes

--- include/simulation/gameobject.h
+++ include/simulation/gameobject.h
@@ -7,34 +7,7 @@
 #include "serializer.h"
 #include "polygon.h"
 #include "utils.h"
-
-class LineStripShape : public sf::Drawable, public sf::Transformable {
-public:
-	explicit LineStripShape();
-	explicit LineStripShape(sf::VertexArray& varray);
-	void setLineColor(sf::Color color);
-	sf::VertexArray varray;
-	void drawMask(sf::RenderTarget& mask, sf::RenderStates states = sf::RenderStates::Default);
-private:
-	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-	sf::Color line_color;
-};
-
-class CircleNotchShape : public sf::Drawable, public sf::Transformable {
-public:
-	explicit CircleNotchShape(float radius, size_t point_count, size_t notch_segment_count);
-	const sf::Color& getCircleColor() const;
-	const sf::Color& getNotchColor() const;
-	void setCircleColor(const sf::Color& color);
-	void setNotchColor(const sf::Color& color);
-	void drawMask(sf::RenderTarget& mask, sf::RenderStates states = sf::RenderStates::Default);
-private:
-	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-	sf::VertexArray varray_circle;
-	sf::VertexArray varray_notch;
-	sf::Color circle_color;
-	sf::Color notch_color;
-};
+#include "shapes.h"
 
 struct BodyDef {
 	b2BodyDef body_def;
--- src/simulation/gameobject.cpp
+++ src/simulation/gameobject.cpp
@@ -866,32 +866,6 @@ bool BallObject::operator==(const BallObject& other) const {
 	return true;
 }
 
-LineStripShape::LineStripShape() { }
-
-LineStripShape::LineStripShape(sf::VertexArray& varray) {
-	this->varray = varray;
-}
-
-void LineStripShape::setLineColor(sf::Color color) {
-	for (size_t i = 0; i < varray.getVertexCount(); i++) {
-		varray[i].color = color;
-	}
-	line_color = color;
-}
-
-void LineStripShape::drawMask(sf::RenderTarget& mask, sf::RenderStates states) {
-	states.transform *= getTransform();
-	sf::Color orig_color = line_color;
-	setLineColor(sf::Color::White);
-	mask.draw(varray, states);
-	setLineColor(orig_color);
-}
-
-void LineStripShape::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-	states.transform *= getTransform();
-	target.draw(varray, states);
-}
-
 PolygonObject::PolygonObject(
 	GameObjectList* object_list,
 	b2BodyDef def,
@@ -1046,60 +1020,6 @@ bool PolygonObject::operator==(const PolygonObject& other) const {
 		return false;
 	}
 	return true;
-}
-
-CircleNotchShape::CircleNotchShape(float radius, size_t point_count, size_t notch_segment_count) {
-	varray_circle = sf::VertexArray(sf::TriangleFan, point_count + 1);
-	varray_notch = sf::VertexArray(sf::TriangleFan, notch_segment_count + 2);
-	float segment_angle = (float)(2 * std::numbers::pi / (float)point_count);
-	float notch_angle = segment_angle * notch_segment_count;
-	float angle_offset = -notch_angle / 2.0f;
-	varray_circle[0] = sf::Vertex(sf::Vector2f(0.0f, 0.0f));
-	for (size_t i = 0; i < point_count; i++) {
-		sf::Vector2f pos = utils::get_circle_vertex<sf::Vector2f>(i, point_count, radius, angle_offset);
-		varray_circle[i] = sf::Vertex(pos);
-	}
-	varray_notch[0] = sf::Vertex(sf::Vector2f(0.0f, 0.0f));
-	for (size_t i = 0; i < notch_segment_count + 1; i++) {
-		sf::Vector2f pos = utils::get_circle_vertex<sf::Vector2f>(i, point_count, radius, angle_offset);
-		varray_notch[i] = sf::Vertex(pos);
-	}
-}
-
-const sf::Color& CircleNotchShape::getCircleColor() const {
-	return circle_color;
-}
-
-const sf::Color& CircleNotchShape::getNotchColor() const {
-	return notch_color;
-}
-
-void CircleNotchShape::setCircleColor(const sf::Color& color) {
-	for (size_t i = 0; i < varray_circle.getVertexCount(); i++) {
-		varray_circle[i].color = color;
-	}
-	circle_color = color;
-}
-
-void CircleNotchShape::setNotchColor(const sf::Color& color) {
-	for (size_t i = 0; i < varray_notch.getVertexCount(); i++) {
-		varray_notch[i].color = color;
-	}
-	notch_color = color;
-}
-
-void CircleNotchShape::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-	states.transform *= getTransform();
-	target.draw(varray_circle, states);
-	target.draw(varray_notch, states);
-}
-
-void CircleNotchShape::drawMask(sf::RenderTarget& mask, sf::RenderStates states) {
-	states.transform *= getTransform();
-	sf::Color orig_color = circle_color;
-	setCircleColor(sf::Color::White);
-	mask.draw(varray_circle, states);
-	setCircleColor(orig_color);
 }
 
 ChainObject::ChainObject(GameObjectList* object_list, b2BodyDef def, std::vector<b2Vec2> p_vertices, sf::Color color) {
