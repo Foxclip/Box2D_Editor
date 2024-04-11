4d9aa910d4a81c4324aefe561a08e5252e31dae8
Foxclip <destroyApp2@gmail.com>
2024-04-11 18:45:21 +1000

    Tests: Simulation: set_position_three test

--- src/tests/simulation_tests.cpp
+++ src/tests/simulation_tests.cpp
@@ -570,6 +570,36 @@ void SimulationTests::createGameObjectList() {
             tVec2ApproxCompare(box1->getGlobalPosition(), box1_new_global_pos);
         }
     );
+    test::Test* set_position_three_test = list->addTest(
+        "set_position_three",
+        {
+            set_parent_two_test,
+            set_position_two_test
+        },
+        [&](test::Test& test) {
+        Simulation simulation;
+        b2Vec2 box0_initial_pos = b2Vec2(0.5f, 0.5f);
+        b2Vec2 box1_initial_pos = b2Vec2(1.1f, 1.1f);
+        b2Vec2 box2_initial_pos = b2Vec2(1.75f, 1.75f);
+        BoxObject* box0 = createBox(simulation, "box0", box0_initial_pos);
+        BoxObject* box1 = createBox(simulation, "box1", box1_initial_pos);
+        BoxObject* box2 = createBox(simulation, "box2", box2_initial_pos);
+        b2Vec2 box1_box0_rel_pos = box1_initial_pos - box0_initial_pos;
+        b2Vec2 box2_box1_rel_pos = box2_initial_pos - box1_initial_pos;
+        b2Vec2 box0_new_pos(1.0f, 1.0f);
+        box1->setParent(box0);
+        box2->setParent(box1);
+        box0->setPosition(box0_new_pos);
+        tVec2ApproxCompare(box0->getGlobalPosition(), box0_new_pos);
+        tVec2ApproxCompare(box1->getGlobalPosition(), box0_new_pos + box1_box0_rel_pos);
+        tVec2ApproxCompare(box2->getGlobalPosition(), box0_new_pos + box1_box0_rel_pos + box2_box1_rel_pos);
+        b2Vec2 box1_new_pos(2.0f, 2.0f);
+        box1->setPosition(box1_new_pos);
+        tVec2ApproxCompare(box0->getGlobalPosition(), box0_new_pos);
+        tVec2ApproxCompare(box1->getGlobalPosition(), box0_new_pos + box1_new_pos);
+        tVec2ApproxCompare(box2->getGlobalPosition(), box0_new_pos + box1_new_pos + box2_box1_rel_pos);
+    }
+    );
 }
 
 std::string SimulationTests::colorToStr(const sf::Color& color) {
