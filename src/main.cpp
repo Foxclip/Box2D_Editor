655147ec4c6689f26509b7b06ff734848f6fae20
Foxclip <destroyApp2@gmail.com>
2025-02-08 16:18:46 +1000

    Outliner: fix selecting multiple entries

--- src/main.cpp
+++ src/main.cpp
@@ -31,9 +31,6 @@ int main() {
 
     execute_app();
 
-    // TODO: TreeViewWidget: fix target highlight position
-    // TODO: TreeViewWidget: fix drag cancelling with Esc
-    // TODO: TreeViewEntry: remove grab_begin
     // TODO: Outliner: reorder objects
     // TODO: Outliner: buttons up/down for reordering objects
     // TODO: Outliner: reparent objects
